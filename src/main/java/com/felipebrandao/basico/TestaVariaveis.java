7cbb56f3606fdfb09edcd0dc685188fae62c1560
Felipe Brandão Cordeiro <desenv07@rommanel.com.br>
2020-01-08 13:09:40 -0300

    Primeiro commit

---
+++ src/main/java/com/felipebrandao/basico/TestaVariaveis.java
@@ -0,0 +1,13 @@
+package com.felipebrandao.basico;
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+	
+		System.out.println("ola novo teste");
+
+		int idade = 37;
+
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+}
